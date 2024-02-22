ab84127f6b1eae09a0e41d5746b371c9b3cb424e
Rolandas <rolarola631@gmail.com>
2024-02-22 21:08:41 +0200

    initial commit

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,10 @@ import Navbar from '../navbar/Navbar';
 import logo from '../../assets/images/auto-logo-without-bg.png';
 import './Header.scss';
 import { useState } from 'react';
+
 import { BiMenuAltRight } from 'react-icons/bi';
 import { AiOutlineClose } from 'react-icons/ai';
+import { Link } from 'react-router-dom';
 function Header() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
@@ -14,8 +16,11 @@ function Header() {
     return (
         <header className="header">
             <div className="logo-div">
-                <img className="logo-no-background" src={logo} alt="Logo" />
+                <Link to="/">
+                    <img className="logo-no-background" src={logo} alt="Logo" />
+                </Link>
             </div>
+
             <Navbar isMobileMenuOpen={isMobileMenuOpen} />
             <div className="app-mobilescreen">
                 {isMobileMenuOpen ? (
